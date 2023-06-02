3038c2b4d5e69f372ca41c562dec14f0b1423e57
Arnav Nayak <arnavnayak2003@gmail.com>
2023-06-02 00:10:13 -0400

    backend secure

--- engine/firebase.py
+++ engine/firebase.py
@@ -1,9 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
+from dotenv import load_dotenv
 
-cred = credentials.Certificate(
-    "fouralpha-b77bd-firebase-adminsdk-u44he-3944c76ddb.json"
-)
+load_dotenv()
+
+path = os.getenv("FIREBASE_CONFIG_PATH")
+cred = credentials.Certificate(path)
 app = firebase_admin.initialize_app(cred)
 firestore_client = firestore.client()
